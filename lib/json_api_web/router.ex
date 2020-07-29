38a5c94ff563a5a12e63dc73db1f81fee229c665
Attila Blascsák <attila.blascsak@gmail.com>
2020-07-29 21:52:27 +0200

    Fix broken user tests

--- lib/json_api_web/router.ex
+++ lib/json_api_web/router.ex
@@ -3,13 +3,23 @@ defmodule JsonAPIWeb.Router do
 
   pipeline :api do
     plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :api_auth do
+    plug :ensure_authenticated
   end
 
   scope "/api", JsonAPIWeb do
     pipe_through :api
 
-    resources "/users", UserController, except: [:new, :edit]
     post "/users/sign_in", UserController, :sign_in
+  end
+
+  scope "/api", JsonAPIWeb do
+    pipe_through [:api, :api_auth]
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enables LiveDashboard only for development
@@ -25,6 +35,20 @@ defmodule JsonAPIWeb.Router do
     scope "/" do
       pipe_through [:fetch_session, :protect_from_forgery]
       live_dashboard "/dashboard", metrics: JsonAPIWeb.Telemetry
+    end
+  end
+
+  defp ensure_authenticated(conn, _opts) do
+    current_user_id = get_session(conn, :current_user_id)
+
+    if current_user_id do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> put_view(JsonAPIWeb.ErrorView)
+      |> render("401.json", message: "Unauthenticated user")
+      |> halt()
     end
   end
 end
