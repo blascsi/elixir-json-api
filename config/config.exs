0b4e10751dbf24a77f684e3fbf466557604514a1
Attila Blascsák <attila.blascsak@gmail.com>
2020-07-29 18:12:09 +0200

    Initial commit

---
+++ config/config.exs
@@ -0,0 +1,33 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :json_api,
+  namespace: JsonAPI,
+  ecto_repos: [JsonAPI.Repo],
+  generators: [binary_id: true]
+
+# Configures the endpoint
+config :json_api, JsonAPIWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "iq2kPI6BjBwslmR1VaQ9ttRyOr27PidRpjcISKg2DyA/dIfb9gcj/jqwfaThNwAj",
+  render_errors: [view: JsonAPIWeb.ErrorView, accepts: ~w(json), layout: false],
+  pubsub_server: JsonAPI.PubSub,
+  live_view: [signing_salt: "U5jpadPZ"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
---
+++ lib/json_api/repo.ex
@@ -0,0 +1,5 @@
+defmodule JsonAPI.Repo do
+  use Ecto.Repo,
+    otp_app: :json_api,
+    adapter: Ecto.Adapters.Postgres
+end
