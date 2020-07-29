af00e20b532a2fc6a22843014f8c7d71b788e9ac
Attila Blascsák <attila.blascsak@gmail.com>
2020-07-29 21:10:25 +0200

    Add a seed data for users

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,8 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+IO.puts("Seeding users...")
+
+JsonAPI.Account.create_user(%{email: "user1@example.com", password: "asdQWE123"})
+JsonAPI.Account.create_user(%{email: "user2@example.com", password: "asdQWE123"})
